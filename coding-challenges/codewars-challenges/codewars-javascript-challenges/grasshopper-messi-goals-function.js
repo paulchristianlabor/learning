8efc436347571f3f6f88cf9e0b72ed3c9c353ec9
paulchristianlabor <paulchristianlaborofficial@gmail.com>
2024-09-20 00:12:26 +0800

    Adding the solution to Code Wars Coding Challenges in Javascript

---
+++ coding-challenges/codewars-challenges/codewars-javascript-challenges/grasshopper-messi-goals-function.js
@@ -0,0 +1,38 @@
+// Grasshopper - Messi goals function
+
+/*
+Instruction:
+
+Messi goals function
+
+Messi is a soccer player with goals in three leagues:
+
+LaLiga
+Copa del Rey
+Champions
+Complete the function to return his total number of goals in all three leagues.
+
+Note: the input will always be valid.
+
+For example:
+
+5, 10, 2  -->  17
+*/
+
+// Code
+
+/*
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  // code goes here
+}
+
+*/
+
+// My Code Starts Here
+
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    // code goes here
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+goals(5, 10, 2);
---
+++ coding-challenges/codewars-challenges/codewars-javascript-challenges/multiply.js
@@ -0,0 +1,26 @@
+// Multiply
+
+/*
+
+Instruction:
+
+This code does not execute properly. Try to figure out why.
+
+*/
+
+// Code
+
+/*
+
+function multiply(a, b){
+  a * b
+}
+
+*/
+
+/* My Code Starts Here Below */
+
+function multiply(a, b) {
+    return a * b;
+}
+multiply(1, 2);
